d0f3f54e7ab69b0cf1cd2ce0d554c096830a9c00
ShuntaOkuhara <115977529+Jamese-P@users.noreply.github.com>
2022-11-04 17:20:27 +0900

    Update stop_watch.vhd

--- stop_watch.vhd
+++ stop_watch.vhd
@@ -12,10 +12,10 @@ end stop_watch;
 architecture rtl of stop_watch is
 	type state_type is (s0, s1, s2, s3);
 	signal state : state_type;
-	signal count : std_logic_vector(2 downto 0);
+	signal count : std_logic_vector(19 downto 0);
 	signal sq1, sq2, sq3, sq4 : std_logic_vector(3 downto 0);
 begin
-	state <= s0;
+	--state <= s0;
 	process (xrst, clk, button)
 	begin
 		if (xrst = '0') then
@@ -24,19 +24,25 @@ begin
 
 		case state is
 			when s0 =>
-				if (button = '0') then
+				if ( button = '0') then
 					state <= s1;
 				else
 					state <= state;
 				end if;
 			when s1 =>
-				if (button = '0') then
+				if ( button = '1') then
 					state <= s2;
 				else
 					state <= state;
 				end if;
 			when s2 =>
-				if (button = '0') then
+				if ( button = '0') then
+					state <= s3;
+				else
+					state <= state;
+				end if;
+when s3 =>
+				if ( button = '0') then
 					state <= s1;
 				else
 					state <= state;
@@ -49,30 +55,33 @@ begin
 	process (clk)
 	begin
 		if (state = s0) then
-			count <= "000";
+			count <= "00000000000000000000";
 			sq4 <= "0000";
 			sq3 <= "0000";
 			sq2 <= "0000";
 			sq1 <= "0000";
-		elsif (state = s1) then
+		elsif (state = s2) then
 			if (clk'event and clk = '1') then
 				count <= count + 1;
-				if (count = "111") then
+				if (count = "11110100001001000000") then
 					sq1 <= sq1 + 1;
-					count <= "000";
-				end if;
-				if (sq1 = "1010") then
+					count <= "00000000000000000000";
+				
+				if (sq1 = "1001") then
 					sq2 <= sq2 + 1;
 					sq1 <= "0000";
-				end if;
-				if (sq2 = "1010") then
+				
+				if (sq2 = "1001") then
 					sq3 <= sq3 + 1;
 					sq2 <= "0000";
-				end if;
-				if (sq3 = "1010") then
+				
+				if (sq3 = "1001") then
 					sq4 <= sq4 + 1;
 					sq3 <= "0000";
 				end if;
+end if;
+end if;
+end if;
 
 			end if;
 		end if;
