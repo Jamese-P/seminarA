f5c8331dc530264e1ca0b7d16d7406960a48482a
ShuntaOkuhara <115977529+Jamese-P@users.noreply.github.com>
2023-06-23 14:40:37 +0900

    6/23

--- pose2.py
+++ pose2.py
@@ -5,6 +5,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
+i=0
 # Webカメラから入力
 cap = cv2.VideoCapture(1)
 with mp_pose.Pose(
@@ -19,7 +20,6 @@ with mp_pose.Pose(
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = pose.process(image)
 
-
     rec1=(50,80)
     rec2=(rec1[0]+100,rec1[1]+100)
 
@@ -29,7 +29,7 @@ with mp_pose.Pose(
     width=cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height=cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     #(1280,720)
-    x_left=results.pose_landmarks.landmark[19].x * width
+    x_left=results.pose_landmarks.landmark[19].x*width
     y_left=results.pose_landmarks.landmark[19].y*height
     x_right=results.pose_landmarks.landmark[20].x*width
     y_right=results.pose_landmarks.landmark[20].y*height
@@ -49,13 +49,21 @@ with mp_pose.Pose(
 
     if (rec1[0]<x_left) and (rec2[0]>x_left) and(rec1[1]<y_left) and (rec2[1]>y_left):
       #print("left touch")
-      cv2.putText(image, 'left touch', xy_string, cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
+      cv2.putText(image, 'left touch', (int(x_left),int(y_left)), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
     if (rec1[0]<x_right) and (rec2[0]>x_right) and(rec1[1]<y_right) and (rec2[1]>y_right):
       #print("right touch")
-      cv2.putText(image, 'right touch', xy_string, cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
+      cv2.putText(image, 'right touch', (int(x_right),int(y_right)), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
 
     #cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
     cv2.imshow('MediaPipe Pose', image)
     if cv2.waitKey(5) & 0xFF == 27:
       break
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    out = cv2.VideoWriter(f'output/sample.mp4', cv2.VideoWriter_fourcc('M','J','P','G'), fps, (width, height))
+    if i<1000:
+      i=i+1
+    else:
+      break
+out.release()
 cap.release()
