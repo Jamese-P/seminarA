9a46b95dd5a4ebf12f34ef79dfca7fedae846ae1
ShuntaOkuhara <115977529+Jamese-P@users.noreply.github.com>
2023-06-24 10:59:09 +0900

    preserve video

--- pose2.py
+++ pose2.py
@@ -1,13 +1,23 @@
 import cv2
 import mediapipe as mp
 import random
+import time
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-i=0
+time_sta=time.time()
+time_pre=time.time()
 # Webカメラから入力
 cap = cv2.VideoCapture(1)
+fps = cap.get(cv2.CAP_PROP_FPS)
+width=cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height=cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+out = cv2.VideoWriter('output/sample.mp4', cv2.VideoWriter_fourcc('M','P','4','V'), 10, (int(width), int(height)))
+
+rec1=(random.randint(1,width-100),random.randint(1,height-100))
+rec2=(rec1[0]+100,rec1[1]+100)
+
 with mp_pose.Pose(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as pose:
@@ -20,14 +30,14 @@ with mp_pose.Pose(
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = pose.process(image)
 
-    rec1=(50,80)
-    rec2=(rec1[0]+100,rec1[1]+100)
+    if (time.time()-time_pre)>5:
+      rec1=(random.randint(1,width-100),random.randint(1,height-100))
+      rec2=(rec1[0]+100,rec1[1]+100)
+      time_pre=time.time()
 
     #図形を描画
     cv2.rectangle(image, rec1,rec2, (0, 255, 0), thickness=8)
 
-    width=cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    height=cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     #(1280,720)
     x_left=results.pose_landmarks.landmark[19].x*width
     y_left=results.pose_landmarks.landmark[19].y*height
@@ -49,21 +59,20 @@ with mp_pose.Pose(
 
     if (rec1[0]<x_left) and (rec2[0]>x_left) and(rec1[1]<y_left) and (rec2[1]>y_left):
       #print("left touch")
-      cv2.putText(image, 'left touch', (int(x_left),int(y_left)), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
+      cv2.putText(image, 'left touch', (int(width-x_left),int(y_left)), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
     if (rec1[0]<x_right) and (rec2[0]>x_right) and(rec1[1]<y_right) and (rec2[1]>y_right):
       #print("right touch")
-      cv2.putText(image, 'right touch', (int(x_right),int(y_right)), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
+      cv2.putText(image, 'right touch', (int(width-x_right),int(y_right)), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
 
+    out.write(image)
     #cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
     cv2.imshow('MediaPipe Pose', image)
+
     if cv2.waitKey(5) & 0xFF == 27:
       break
-
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    out = cv2.VideoWriter(f'output/sample.mp4', cv2.VideoWriter_fourcc('M','J','P','G'), fps, (width, height))
-    if i<1000:
-      i=i+1
-    else:
+    
+    if (time.time()-time_sta)>30:
+      print("finish record")
       break
 out.release()
 cap.release()
